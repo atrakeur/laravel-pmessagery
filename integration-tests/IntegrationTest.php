a55369d0a40c18446bf2873f18b2486ff50dd8d9
atrakeur <atrakeur@gmail.com>
2014-03-27 00:42:26 +0100

    Setup integration tests

---
+++ integration-tests/IntegrationTest.php
@@ -0,0 +1,21 @@
+<?php
+
+abstract class IntegrationTest extends Illuminate\Foundation\Testing\TestCase {
+
+	/**
+	 * Creates the application.
+	 *
+	 * @return \Symfony\Component\HttpKernel\HttpKernelInterface
+	 */
+	public function createApplication()
+	{
+		$unitTesting = true;
+
+		$testEnvironment = 'testing';
+
+		return require __DIR__.'/../../bootstrap/start.php';
+	}
+
+	
+
+}
--- src/Atrakeur/Pmessagery/Controllers/MessageryBaseController.php
+++ src/Atrakeur/Pmessagery/Controllers/MessageryBaseController.php
@@ -1,12 +1,9 @@
 <?php namespace Atrakeur\Pmessagery\Controllers;
 
-use \Pmessagery;
-
 class MessageryBaseController extends \BaseController {
 
-	public function inbox() {
-		Pmessagery::test();
-		echo 'ok';
+	public function conversationList() {
+		
 	}
 
 }
